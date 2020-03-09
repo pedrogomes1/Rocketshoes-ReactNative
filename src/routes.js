e1b6c3255662996ca0619aef00c3aab0504bbb81
Pedro <pedro-gomes17@hotmail.com>
2020-03-09 07:39:24 -0300

    Arquivo de rotas

---
+++ src/routes.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+
+import Header from './components/Header';
+
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Home,
+      Cart,
+    },
+    {
+      defaultNavigationOptions: navigation => ({
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        header: <Header {...navigation} />,
+      }),
+    }
+  )
+);
+
+export default Routes;
